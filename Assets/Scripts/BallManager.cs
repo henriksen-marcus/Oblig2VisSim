955a095e1215f116057a5a1e4af92d29373c7e7e
Marcus Henriksen <89453098+henriksen-marcus@users.noreply.github.com>
2023-11-23 16:55:08 +0100

    Ball pool auto adjusting size

--- Assets/Scripts/BallManager.cs
+++ Assets/Scripts/BallManager.cs
@@ -12,8 +12,14 @@ public class BallManager : MonoBehaviour
     
     [SerializeField] private GameObject ballPrefab;
     [SerializeField] private float ballDespawnY = -50f;
+    /// <summary>
+    /// If more balls than this exist in the pool,they get destroyed after going inactive.
+    /// </summary>
+    [SerializeField] private int maxDesiredBallsInPool = 20;
     
-    // Ball pool. We use this to avoid creating and destroying balls.
+    /// <summary>
+    /// Ball pool. We use this to avoid creating and destroying balls.
+    /// </summary>
     List<BallPhysics> balls = new();
 
     private void Awake()
@@ -29,15 +35,32 @@ public class BallManager : MonoBehaviour
     
     void Update()
     {
+        List<BallPhysics> ballsToRemove = new();
+        int numBallsToRemove = balls.Count - maxDesiredBallsInPool;
+        int numBallsRemoved = 0;
         foreach (var ball in balls)
         {
             if (ball.transform.position.y > ballDespawnY || !ball.gameObject.activeSelf)
                 continue;
-            
-            ball.gameObject.SetActive(false);
-            ball.Reset();
-            print("Set to inactive");
+
+            if (numBallsRemoved < numBallsToRemove)
+            {
+                ballsToRemove.Add(ball);
+                numBallsRemoved++;
+            }
+            else
+            {
+                ball.gameObject.SetActive(false);
+                ball.Reset();
+            }
             break;
+        }
+        //print("deleting " + ballsToRemove.Count + " balls");
+        // Remove balls from the pool
+        foreach (var ball in ballsToRemove)
+        {
+            balls.Remove(ball);
+            Destroy(ball.gameObject);
         }
     }
 
@@ -49,11 +72,11 @@ public class BallManager : MonoBehaviour
             if (ball.gameObject.activeSelf) continue;
             ball.transform.position = position;
             ball.gameObject.SetActive(true);
-            print("Balls in the pool: " + balls.Count());
+            //print("Balls in the pool: " + balls.Count());
             return;
         }
         var instantiated = Instantiate(ballPrefab, position, Quaternion.identity).GetComponent<BallPhysics>();
         balls.Add(instantiated);
-        print("Balls in the pool: " + balls.Count());
+        //print("Balls in the pool: " + balls.Count());
     }
 }
