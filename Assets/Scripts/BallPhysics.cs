981e0e86fd05f156cab716c05cfcec79c1b9568a
Marcus Henriksen <89453098+henriksen-marcus@users.noreply.github.com>
2023-11-27 20:48:07 +0100

    Balls drawing splines

--- Assets/Scripts/BallPhysics.cs
+++ Assets/Scripts/BallPhysics.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.Animations;
@@ -9,18 +10,36 @@ public class BallPhysics : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] [Range(0,1)] float bounciness = 0;
+    [SerializeField] private bool drawSplinePath = false;
+    [SerializeField] private float distanceBetweenSplinePoints = 15f;
     
     [Header("Debug")] 
     [SerializeField] private bool showDebugSphere = true;
 
-    public TriangleSurface triangleSurface;
-    
+    /// <summary>
+    /// Gravitational acceleration.
+    /// </summary>
     private Vector3 g = Physics.gravity;
+    /// <summary>
+    /// Mass.
+    /// </summary>
     private float m = 1f;
-    private float r = 0.02f;
+    /// <summary>
+    /// Radius.
+    /// </summary>
+    private float r;
+    /// <summary>
+    /// Sphere trigger that checks for overlapping balls.
+    /// </summary>
+    private SphereCollider trigger;
     private Vector3 velocity = Vector3.zero;
-    private Vector3 prevNormal = Vector3.zero;
-    private Vector3 lastGivenPos = Vector3.zero;
+    private Vector3 lastCollPosition = Vector3.zero;
+    private List<BallPhysics> affectingBalls = new List<BallPhysics>();
+    private BSpline splinePath;
+    private Vector3 lastSplinePoint;
+    private LineRenderer lineRenderer;
+    private TriangleSurface triangleSurface;
+    private bool hasBegunTracingPath = false;
 
     // Start is called before the first frame update
     void Start()
@@ -28,40 +47,49 @@ public class BallPhysics : MonoBehaviour
         if (triangleSurface == null)
             triangleSurface = WorldManager.Instance.triangleSurface;
         
+        // Initialize radius
         r = transform.localScale.x / 2;
+
+        trigger = GetComponent<SphereCollider>();
+        if (trigger == null)
+            print("SHIT");
+        
+        splinePath = GetComponent<BSpline>();
+        lineRenderer = GetComponent<LineRenderer>();
     }
 
     private void FixedUpdate()
     {
         Vector3 pos = transform.position;
         Vector2 pos2D = new Vector2(pos.x, pos.z);
+        
+        // Get collision information
         var hit = triangleSurface.GetCollision(pos2D);
-
-        Vector3 newVel = velocity;
+        CorrectCollisionToSurface(ref hit);
+        
         Vector3 N = new Vector3();
         Vector3 G = m * g;
         Vector3 normalVelocity;
         
-        CorrectCollisionToSurface(ref hit);
-        
+        // Check if we are at the same height as the surface
         float dist = Vector3.Distance(hit.Position, pos);
         bool validY = dist <= r;
         
         if (hit.isHit && validY)
         {
             normalVelocity = Vector3.Dot(velocity, hit.Normal) * hit.Normal;
-            
             // Reflection
             velocity = velocity - normalVelocity - bounciness * normalVelocity;
-            
-            lastGivenPos = hit.Position;
-            
+            lastCollPosition = hit.Position;
             N = -Vector3.Dot(hit.Normal, G) * hit.Normal;
-
+            
             // Move ball up to surface
             transform.position = hit.Position + r * hit.Normal;
+            
+            // Enable this once we hit the surface
+            hasBegunTracingPath = true;
         }
-        else lastGivenPos = Vector3.zero;
+        else lastCollPosition = Vector3.zero;
 
         var accel = (G + N) / m;
 
@@ -71,15 +99,43 @@ public class BallPhysics : MonoBehaviour
         //print($"Position: {transform.position}\nVelocity: {velocity}\nAcceleration: {accel}\nNormal: {hit.Normal}");
     }
 
+    private void Update()
+    {
+        if (!hasBegunTracingPath) return;
+        
+        // Add position to spline path
+        if (lastSplinePoint == Vector3.zero)
+        {
+            lastSplinePoint = transform.position;
+            splinePath.AddPoint(lastSplinePoint);
+        }
+        // Only add point if we have moved a significant distance, to avoid wasting performance
+        else if (Vector3.Distance(lastSplinePoint, transform.position) > distanceBetweenSplinePoints)
+        {
+            lastSplinePoint = transform.position;
+            splinePath.AddPoint(lastSplinePoint);
+            
+            // Update line renderer if we have enough points
+            int count = splinePath.GetCount();
+            if (count > 2)
+            {
+                var points = splinePath.GetPoints();
+                lineRenderer.positionCount = points.Length;
+                lineRenderer.SetPositions(points);
+            }
+                
+        }
+    }
+
     /// <summary>
     /// Modifies the hit position to accurately reflect the ball's
     /// position if it were to lie on the triangle surface.
     /// </summary>
     private void CorrectCollisionToSurface(ref TriangleSurface.Hit hit)
     {
-        var c = transform.position;
-        Vector3 d = c - hit.Position;
-        hit.Position = c - Vector3.Dot(d, hit.Normal) * hit.Normal;
+        Vector3 p = transform.position;
+        Vector3 d = p - hit.Position;
+        hit.Position = p - Vector3.Dot(d, hit.Normal) * hit.Normal;
     }
 
     private void OnDrawGizmos()
@@ -87,14 +143,25 @@ public class BallPhysics : MonoBehaviour
         if (showDebugSphere)
         {
             Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(lastGivenPos, 2.2f);
+            Gizmos.DrawWireSphere(lastCollPosition, 2.2f);
         }
     }
     
     public void Reset()
     {
         velocity = Vector3.zero;
-        prevNormal = Vector3.zero;
-        lastGivenPos = Vector3.zero;
+        lastCollPosition = Vector3.zero;
+        lastSplinePoint = Vector3.zero;
+        splinePath.Reset();
+        lineRenderer.positionCount = 0;
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Raindrop"))
+        {
+            other.gameObject.SetActive(false);
+            other.GetComponent<BallPhysics>().Reset();
+        }
     }
 }
